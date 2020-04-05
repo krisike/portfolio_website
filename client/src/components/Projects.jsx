a1c40214e94f2481500d7678f23e1aeb16e5fbc8
Krisi <kpucu9@gmail.com>
2020-04-05 14:25:02 -0500

    content of projects page

--- client/src/components/Projects.jsx
+++ client/src/components/Projects.jsx
@@ -1,7 +1,46 @@
 import React from 'react';
+import { allProjects } from '../constants/constants.js';
 
 const Projects = () => {
-  return <div className="projects">Projects</div>;
+  return (
+    <>
+      <div className="hr">
+        <hr />
+      </div>
+      <div className="about">
+        <p>
+          A web developer with enthusiasm and team spirit eager to contribute to
+          the tech world. An environmental activist and social media zealot
+          along the way.
+        </p>
+      </div>
+      <div>
+        {allProjects.map(
+          ({ projectName, Description, gitHubLink, gifLink }, index) => {
+            return (
+              <>
+                <a
+                  key={index}
+                  className="project-link"
+                  href={gitHubLink}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <p>{projectName}</p>
+                </a>
+                <p>{Description}</p>
+                <p>{gitHubLink}</p>
+                <img src={gifLink} alt="project-link" />
+              </>
+            );
+          }
+        )}
+      </div>
+      <div className="hr">
+        <hr />
+      </div>
+    </>
+  );
 };
 
 export default Projects;
--- client/src/constants/constants.js
+++ client/src/constants/constants.js
@@ -1,21 +1,24 @@
-export const allProjects = () => {
-  return [
-    {
-      projectName: 'AirAware',
-      Description:
-        'An online platform where businesses and individuals can follow and compare the air pollution index in the United States.',
-      technologyUsed:
-        'Technology used: React, API, CSS, JavaScript, Material-UI'
-    },
-    {
-      projectName: 'FINDPEACE',
-      Description:
-        'A discrete platform that utilizes Word-of-mouth marketing with a yoga theme via which users can go to get information for shelters and other related organizations in situations of human trafficking or domestic violence. ',
-      technologyUsed:
-        'Technology used: Rails, JavaScript, React, React on Rails, Pure CSS, PostgreSQL, Canva, Adobe XD, Mapbox GL, Axios'
-    }
-  ];
-};
+export const allProjects = [
+  {
+    projectName: 'AirAware',
+    Description:
+      'An online platform where businesses and individuals can follow and compare the air pollution index in the United States.',
+    technologyUsed: 'Technology used: React, API, CSS, JavaScript, Material-UI',
+    websiteLink: 'https://air-aware.herokuapp.com',
+    gitHubLink: 'https://github.com/krisike/AirAware',
+    gifLink: 'https://media.giphy.com/media/f6xuOUAoqEH4I3hzgu/giphy.gif'
+  },
+  {
+    projectName: 'FINDPEACE',
+    Description:
+      'A discrete platform that utilizes Word-of-mouth marketing with a yoga theme via which users can go to get information for shelters and other related organizations in situations of human trafficking or domestic violence. ',
+    technologyUsed:
+      'Technology used: Rails, JavaScript, React, React on Rails, Pure CSS, PostgreSQL, Canva, Adobe XD, Mapbox GL, Axios',
+    websiteLink: 'https://find-peace.herokuapp.com',
+    gitHubLink: 'https://github.com/krisike/findpeace',
+    gifLink: 'https://media.giphy.com/media/XHH054KGFGi5Kdu2jO/giphy.gif'
+  }
+];
 
 export const aboutKrisi = [
   {
