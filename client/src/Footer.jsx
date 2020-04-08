1bfb6076d41efbac0d3bbef2f3c71e7ecd7dbbaa
Krisi <kpucu9@gmail.com>
2020-04-08 02:22:03 -0500

    fixed footer and more styling and resume

--- client/src/Footer.jsx
+++ client/src/Footer.jsx
@@ -1,19 +1,19 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 import ResumeButton from '../src/components/ResumeButton';
 
 const Footer = () => {
   return (
-    <div className="container_footer">
-      <ul className="menu_footer">
+    <div className="containerFooter">
+      <ul className="menuFooter">
         <li>
           <ResumeButton />
         </li>
-        <li>
+        {/* <li>
           <Link to="/contact" className="contact_link">
             Get in Touch
           </Link>
-        </li>
+        </li> */}
       </ul>
     </div>
   );
