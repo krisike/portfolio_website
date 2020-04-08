6dd55fc8027a9f4b61b595932597e850a1806593
Krisi <kpucu9@gmail.com>
2020-04-08 13:26:25 -0500

    add catchall handler for heroku

--- server.js
+++ server.js
@@ -23,6 +23,11 @@ app.get('/api/demo', (request, response) => {
   });
 });
 // END DEMO
+// The "catchall" handler: for any request that doesn't
+// match one above, send back React's index.html file.
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/client/build/index.html'));
+});
 
 const port = process.env.PORT || 8080;
 app.listen(port, () => {
