705b77a50dac1869051fcf48fb0849f235d6998b
Krisi <kpucu9@gmail.com>
2020-04-13 22:40:29 -0500

    added hamburger icon font awesome

--- client/src/Header.jsx
+++ client/src/Header.jsx
@@ -1,5 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import 'font-awesome/css/font-awesome.min.css';
+
+const toggleMenu = e => {
+  let currHeight = e.target.parentElement.style.height;
+  e.target.parentElement.style.height = currHeight === 'auto' ? '40px' : 'auto';
+};
 
 const Header = () => {
   return (
@@ -28,6 +34,9 @@ const Header = () => {
             </Link>
           </li>
         </ul>
+      </div>
+      <div className="h-menu" onClick={toggleMenu}>
+        <i className="fa fa-bars"></i>
       </div>
     </div>
   );
